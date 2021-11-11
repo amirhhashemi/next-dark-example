6ff18fcbd950e347066f5038596fa96a34bea758
Arman.H <ahhdev@gmail.com>
2021-11-11 17:12:39 +0330

    setup tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div className="h-screen flex items-center justify-center text-3xl text-bod text-red-500">
+      Hello
+    </div>
+  );
 }
