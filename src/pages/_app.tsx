f9943800f2980a6941df49c6e61758431e83b47c
Arman.H <ahhdev@gmail.com>
2021-11-11 20:11:44 +0330

    diable system prefrences

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
       <Component {...pageProps} />
     </ThemeProvider>
   );
