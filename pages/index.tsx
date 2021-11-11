5590333d683d032737b1ab91a3637ec66530d2bb
Arman.H <ahhdev@gmail.com>
2021-11-11 18:01:32 +0330

    change colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import { ThemeToggleButton } from "./ThemeToggleButton";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center text-3xl text-bod text-red-500 dark:bg-blue-900 dark:text-white">
-      <ThemeToggleButton />
-      Hello
+    <div className="h-screen flex items-center justify-center dark:bg-gray-900">
+      <div className="text-3xl text-bod text-gray-900 dark:text-white">
+        <ThemeToggleButton />
+        Hello
+      </div>
     </div>
   );
 }
